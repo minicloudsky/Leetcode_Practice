83beff6f7a1cf28cd7c0124683508a204f382469
yawuplus <1397991131@qq.com>
2020-03-21 00:36:34 +0800

    括号匹配

--- minicloudsky/contains-duplicate-ii.py
+++ minicloudsky/contains-duplicate-ii.py
@@ -28,9 +28,9 @@ class Solution(object):
         :type k: int
         :rtype: bool
         """
-        for i in range(len(nums)):
-            for j in range(max(i - k), i):
-                if nums[i] == nums[j]:
+        for i in range(len(nums) - 1):
+            for j in range(i + 1, len(nums)):
+                if nums[i] == nums[j] and abs(i - j) < k:
                     return True
         return False
 
